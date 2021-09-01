7001d89fc7ad327482ae4b28f699f939c3095625
naveen.verma <naveen.verma@tothenew.com>
2021-09-01 13:25:50 +0530

    Modules, @Binds and @Provides

--- app/src/main/java/com/example/hilt_demo/MainActivity.kt
+++ app/src/main/java/com/example/hilt_demo/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.hilt_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.scopes.FragmentScoped
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
+import org.jetbrains.annotations.NotNull
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -23,13 +30,13 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-@FragmentScoped
+/*@FragmentScoped
 @AndroidEntryPoint
 class MyFragment: Fragment() {
 
     @Inject
     lateinit var someClass: SomeClass
-}
+}*/
 
 //@Singleton
 //@ActivityScoped
@@ -42,9 +49,8 @@ constructor(
     // Also we can't directly inject the Third-party libraries in hilt :: Need a work around for this
     private val gson: Gson
 ) {
-
     fun doAThing(): String {
-        return "Look I got: ${someInterfaceImpl.getAThing()}"
+        return "Look I got: ${someInterfaceImpl.getAThing()} and \"${gson.javaClass.canonicalName}\" object!"
     }
 }
 
@@ -58,6 +64,67 @@ constructor() : SomeInterface {
 }
 
 interface SomeInterface {
-
     fun getAThing(): String
+}
+
+interface GsonInterface {
+    @NotNull
+    fun getGson(): Gson
+}
+
+
+/**
+ * complex way to create a Module dependencies
+ */
+
+
+/*
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class MyModuleWithApplication {
+
+    @Singleton
+    @Binds
+    abstract fun bindSomeDependency(someInterfaceImpl: SomeInterfaceImpl): SomeInterface
+}
+*/
+
+
+/*@InstallIn(ActivityComponent::class)
+@Module
+abstract class MyModuleWithActivity {
+
+    @ActivityScoped
+    @Binds
+    abstract fun bindSomeDependency(someInterfaceImpl: SomeInterfaceImpl): SomeInterface
+
+    *//**
+     * will not work
+     *//*
+    *//*
+    @ActivityScoped
+    @Binds
+    abstract fun bindGson(gson: Gson): GsonInterface
+     *//*
+}*/
+
+/**
+ * easy way to create a Module dependencies
+ */
+
+@InstallIn(ActivityComponent::class)
+@Module
+class MyModule {
+
+    @ActivityScoped
+    @Provides
+    fun provideSomeInterface(): SomeInterface {
+        return SomeInterfaceImpl()
+    }
+
+    @ActivityScoped
+    @Provides
+    fun provideGson(): Gson {
+        return  Gson()
+    }
 }

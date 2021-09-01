be361d3227423d7f6f08b83bb1f9b8477b8210f0
naveen.verma <naveen.verma@tothenew.com>
2021-09-01 10:44:47 +0530

    Initial commit : Added Dependencies for Hilt

---
+++ app/src/main/java/com/example/hilt_demo/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.example.hilt_demo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by Naveen Verma on 01/09/21.
+ * To The New
+ * naveen.verma@tothenew.com
+ */
+
+@HiltAndroidApp
+class MyApplication: Application() {
+
+}
